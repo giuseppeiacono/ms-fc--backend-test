2d6e55e799e2eabfc0a6ff7de170c1987fa21e54
giuseppe <giuseppe.iacono@ixxus.com>
2018-09-10 10:18:11 +0200

    Created tests to check new controllers and services. Refactoring

--- src/test/java/com/scmspain/controller/TweetControllerTest.java
+++ src/test/java/com/scmspain/controller/TweetControllerTest.java
@@ -1,9 +1,14 @@
 package com.scmspain.controller;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scmspain.configuration.TestConfiguration;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.util.List;
 
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,9 +30,13 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestConfiguration.class)
 public class TweetControllerTest {
+
     @Autowired
     private WebApplicationContext context;
     private MockMvc mockMvc;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Before
     public void setUp() {
@@ -37,19 +45,19 @@ public class TweetControllerTest {
 
     @Test
     public void shouldReturn200WhenInsertingAValidTweet() throws Exception {
-        mockMvc.perform(newTweet("Prospect", "Breaking the law"))
+        publishTweet("Prospect", "Breaking the law")
             .andExpect(status().is(201));
     }
 
     @Test
     public void shouldReturn400WhenInsertingAnInvalidTweet() throws Exception {
-        mockMvc.perform(newTweet("Schibsted Spain", "We are Schibsted Spain (look at our home page http://www.schibsted.es/), we own Vibbo, InfoJobs, fotocasa, coches.net and milanuncios. Welcome!"))
+        publishTweet("Schibsted Spain", "We are Schibsted Spain (look at our home page http://www.schibsted.es/), we own Vibbo, InfoJobs, fotocasa, coches.net and milanuncios. Welcome!")
                 .andExpect(status().is(400));
     }
 
     @Test
     public void shouldReturnAllPublishedTweets() throws Exception {
-        mockMvc.perform(newTweet("Yo", "How are you?"))
+        publishTweet("Yo", "How are you?")
                 .andExpect(status().is(201));
 
         MvcResult getResult = mockMvc.perform(get("/tweet"))
@@ -60,10 +68,69 @@ public class TweetControllerTest {
         assertThat(new ObjectMapper().readValue(content, List.class).size()).isEqualTo(1);
     }
 
+    @Test
+    public void listDiscardedTweets_whenAtLeastOneTweetsWasDiscarded_thenReturnThemOrderedByDiscardedDateDescending() throws Exception {
+        // published tweets
+        publishTweet("Schibsted Spain", "How are you?").andExpect(status().is(201));
+        publishTweet("Schibsted Spain", "I'm fine thank you").andExpect(status().is(201));
+        publishTweet("Schibsted Spain", "It's a great day!").andExpect(status().is(201));
+
+        // discarded tweets
+        discardTweet("{\"tweet\": 3}").andExpect(status().isOk());
+        discardTweet("{\"tweet\": 1}").andExpect(status().isOk());
+
+        // test
+        MvcResult getResult = mockMvc.perform(get("/discarded"))
+                .andExpect(status().is(200))
+                .andReturn();
+
+        // assertions
+        String content = getResult.getResponse().getContentAsString();
+        List discardedTweets = new ObjectMapper().readValue(content, List.class);
+        assertThat(discardedTweets.size()).isEqualTo(2);
+        assertThat(((LinkedHashMap) discardedTweets.get(0)).get("id")).isEqualTo(3);
+        assertThat(((LinkedHashMap) discardedTweets.get(1)).get("id")).isEqualTo(1);
+    }
+
+    @Test
+    public void listDiscardedTweets_whenThereAreNoTweetsDiscarded_thenReturnAnEmptyList() throws Exception {
+        // published tweets
+        publishTweet("Schibsted Spain", "How are you?").andExpect(status().is(201));
+        publishTweet("Schibsted Spain", "I'm fine thank you").andExpect(status().is(201));
+
+        // test
+        MvcResult getResult = mockMvc.perform(get("/discarded"))
+                .andExpect(status().is(200))
+                .andReturn();
+
+        // assertions
+        String content = getResult.getResponse().getContentAsString();
+        List discardedTweets = new ObjectMapper().readValue(content, List.class);
+        assertThat(discardedTweets.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void discardTweet_WhenTweetIdIsEmpty_ThenReturnHttp400() throws Exception {
+        publishTweet("Schibsted Spain", "How are you?").andExpect(status().is(201));
+
+        // test
+        discardTweet("").andExpect(status().is(400));
+    }
+
     private MockHttpServletRequestBuilder newTweet(String publisher, String tweet) {
         return post("/tweet")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(format("{\"publisher\": \"%s\", \"tweet\": \"%s\"}", publisher, tweet));
+    }
+
+    private ResultActions publishTweet(final String publisher, final String tweet) throws Exception {
+        return  mockMvc.perform(newTweet(publisher, tweet));
+    }
+
+    private ResultActions discardTweet(final String tweetIdJson) throws Exception {
+        return mockMvc.perform(post("/discarded")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(tweetIdJson));
     }
 
 }
--- src/test/java/com/scmspain/services/TweetServiceTest.java
+++ src/test/java/com/scmspain/services/TweetServiceTest.java
@@ -1,6 +1,7 @@
 package com.scmspain.services;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import com.scmspain.entities.Tweet;
 import org.junit.Before;
@@ -10,6 +11,7 @@ import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TweetServiceTest {
     private EntityManager entityManager;
@@ -29,5 +31,17 @@ public class TweetServiceTest {
         tweetService.publishTweet("Guybrush Threepwood", "I am Guybrush Threepwood, mighty pirate.");
 
         verify(entityManager).persist(any(Tweet.class));
+    }
+
+    @Test
+    public void listDiscardedTweets_WhenDiscardedTweetsAreRequired_ThenCompleteListIsReturned() {
+        TypedQuery query = mock(TypedQuery.class);
+        when(entityManager.createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 AND isDiscarded = TRUE  ORDER BY id DESC", Long.class)).thenReturn(query);
+
+        // test
+        tweetService.listDiscardedTweets();
+
+        // assertions
+        verify(entityManager).createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 AND isDiscarded = TRUE  ORDER BY id DESC", Long.class);
     }
 }
