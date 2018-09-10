5096696ae1b500668e276f172b902cd3ea2d5ddb
giuseppe <giuseppe.iacono@ixxus.com>
2018-09-10 10:24:31 +0200

    Implemented new controllers and services. Refactoring

--- src/main/java/com/scmspain/controller/TweetController.java
+++ src/main/java/com/scmspain/controller/TweetController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.scmspain.controller.command.DiscardTweetCommand;
 import com.scmspain.controller.command.PublishTweetCommand;
 import com.scmspain.entities.Tweet;
 import com.scmspain.services.TweetService;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class TweetController {
@@ -31,10 +33,21 @@ public class TweetController {
         return this.tweetService.listAllTweets();
     }
 
+    @GetMapping("/discarded")
+    public List<Tweet> listDiscardedTweets() {
+        return this.tweetService.listDiscardedTweets();
+    }
+
     @PostMapping("/tweet")
     @ResponseStatus(CREATED)
     public void publishTweet(@RequestBody @Valid PublishTweetCommand publishTweetCommand) {
         this.tweetService.publishTweet(publishTweetCommand.getPublisher(), publishTweetCommand.getTweet());
+    }
+
+    @PostMapping("/discarded")
+    @ResponseStatus(OK)
+    public void discardTweet(@RequestBody @Valid DiscardTweetCommand discardTweetCommand) {
+        this.tweetService.discardTweet(discardTweetCommand.getTweet());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
--- src/main/java/com/scmspain/services/TweetService.java
+++ src/main/java/com/scmspain/services/TweetService.java
@@ -8,6 +8,8 @@ import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import com.scmspain.entities.Tweet;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.boot.actuate.metrics.writer.Delta;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class TweetService {
+
     private EntityManager entityManager;
     private MetricWriter metricWriter;
 
@@ -24,11 +27,11 @@ public class TweetService {
     }
 
     /**
-      Push tweet to repository
-      Parameter - publisher - creator of the Tweet
-      Parameter - text - Content of the Tweet
-      Result - recovered Tweet
-    */
+     Push tweet to repository
+     Parameter - publisher - creator of the Tweet
+     Parameter - text - Content of the Tweet
+     Result - recovered Tweet
+     */
     public void publishTweet(String publisher, String text) {
         Tweet tweet = new Tweet();
         tweet.setTweet(text);
@@ -39,27 +42,53 @@ public class TweetService {
     }
 
     /**
-      Recover tweet from repository
-      Parameter - id - id of the Tweet to retrieve
-      Result - retrieved Tweet
-    */
+     Recover tweet from repository
+     Parameter - id - id of the Tweet to retrieve
+     Result - retrieved Tweet
+     */
     public Tweet getTweet(Long id) {
-      return this.entityManager.find(Tweet.class, id);
+        return this.entityManager.find(Tweet.class, id);
     }
 
     /**
-      Recover tweet from repository
-      Parameter - id - id of the Tweet to retrieve
-      Result - retrieved Tweet
-    */
+     Recover tweet from repository
+     Parameter - id - id of the Tweet to retrieve
+     Result - retrieved Tweet
+     */
     public List<Tweet> listAllTweets() {
-        List<Tweet> result = new ArrayList<Tweet>();
+        List<Tweet> result = new ArrayList<>();
         this.metricWriter.increment(new Delta<Number>("times-queried-tweets", 1));
-        TypedQuery<Long> query = this.entityManager.createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 ORDER BY id DESC", Long.class);
-        List<Long> ids = query.getResultList();
-        for (Long id : ids) {
-            result.add(getTweet(id));
-        }
+        TypedQuery<Long> query = this.entityManager.createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 AND isDiscarded = FALSE  ORDER BY id DESC", Long.class);
+        query.getResultList().forEach(tweetId -> result.add(getTweet(tweetId)));
         return result;
+    }
+
+    /**
+     * Discard the tweet with id {@code tweetId}.
+     * It means that it will not be shown in the published tweet list
+     *
+     * @param tweetId the id of tweet
+     */
+    public void discardTweet(final Long tweetId) {
+        Query query = getSession().createQuery("update Tweet t set t.isDiscarded = :isDiscarded WHERE t.id = :tweetId");
+        query.setParameter("isDiscarded", Boolean.TRUE);
+        query.setParameter("tweetId", tweetId);
+        query.executeUpdate();
+    }
+
+    /**
+     * Recover all discarded tweets and order them by the date it was discarded on in descending order
+     *
+     * @return discarded tweets
+     */
+    public List<Tweet> listDiscardedTweets() {
+        List<Tweet> result = new ArrayList<>();
+        TypedQuery<Long> query = this.entityManager.createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 AND isDiscarded = TRUE  ORDER BY id DESC", Long.class);
+        query.getResultList().forEach(tweetId -> result.add(getTweet(tweetId)));
+        return result;
+    }
+
+    private Session getSession() {
+        return entityManager.unwrap(Session.class);
     }
 }
